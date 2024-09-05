b44ff3ca173a1d7bec34e67c4e82d0c3c2d16c75
pado232 <ehsilver98@naver.com>
2024-09-05 23:10:55 +0900

    projectContext.js 파일 삭제

--- src/index.js
+++ src/index.js
@@ -7,21 +7,15 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./root/store/store";
-import {
-  projectData,
-  ProjectProvider,
-} from "./root/store/reducers/ProjectContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
-        <ProjectProvider projectData={projectData}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </ProjectProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
